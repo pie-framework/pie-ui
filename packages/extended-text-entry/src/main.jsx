da6994224115da9d41345e9934234535cfa2a4e8
edeustace <ed.eustace@gmail.com>
2018-03-23 10:30:34 -0400

    chore(lint): fix lint issues

--- packages/extended-text-entry/src/main.jsx
+++ packages/extended-text-entry/src/main.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import EditableHTML from "@pie-lib/editable-html";
+import React from 'react';
+import EditableHTML from '"@pie-lib/editable-html';
+import PropTypes from 'prop-types';
 
 class Main extends React.Component {
-  constructor (props) {
+
+  static propTypes = {
+    onChange: PropTypes.func.isRequired
+  }
+  constructor(props) {
     super(props);
     this.handleEditableChange = this.handleEditableChange.bind(this);
   }
@@ -10,17 +15,15 @@ class Main extends React.Component {
   handleEditableChange(change) {
     this.props.onChange(change);
   }
-  
+
   render() {
     return (
-      <div>
-        <EditableHTML 
-          onChange={this.handleEditableChange} 
-          markup="" 
-          activePlugins={['bold', 'bulleted-list', 'numbered-list']}/>
-      </div>
-    )
+      <EditableHTML
+        onChange={this.handleEditableChange}
+        markup=""
+        activePlugins={['bold', 'bulleted-list', 'numbered-list']} />
+    );
   }
-};
+}
 
 export default Main;

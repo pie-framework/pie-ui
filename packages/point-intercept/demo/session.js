efb854a47450c34ad9f2381905a15b6186b13a04
Elod-Arpad Szopos <elodszopos@hotmail.com>
2018-04-10 13:26:30 +0300

    feat(point-intercept): demo config file adjustments

--- packages/point-intercept/demo/session.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  { id: '1', points: [{ x: 0, y: 0, label: 'A' }, { x: 1, y: 2, label: 'B' }] },
-  { id: '2', points: [{ x: 0, y: 0, label: 'Foo' }] },
-  { id: '3', points: [{ x: 0, y: 0, label: 'Z' }] }
-];

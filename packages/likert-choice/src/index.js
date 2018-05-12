442116ab6524743144241abfbf2fa323d7e2c480
Harry <hsharma@primoko.com>
2018-05-12 23:34:34 +0530

    Code Refactored

--- packages/likert-choice/src/index.js
+++ packages/likert-choice/src/index.js
@@ -1,103 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    ModelSetEvent,
+    SessionChangedEvent
+} from '@pie-framework/pie-player-events';
 import Main from './main';
-import debounce from 'lodash/debounce';
-import debug from 'debug';
 
-const log = debug('pie-elements:multiple-choice');
-
-export default class MultipleChoice extends HTMLElement {
-
+export default class RootInlineChoice extends HTMLElement {
     constructor() {
         super();
         this._model = null;
         this._session = null;
-
-        this._rerender = debounce(() => {
+        this._rerender = () => {
             if (this._model && this._session) {
-                var element = React.createElement(Main,
-                    {
-                        model: this._model,
-                        session: this._session,
-                        onChoiceChanged: this._onChange.bind(this)
-                    });
-                ReactDOM.render(element, this);
-
-            } else {
-                log('skip');
+                let elem = React.createElement(Main, {
+                    model: this._model,
+                    session: this._session,
+                    onChoiceChanged: this._handleChoiceChange.bind(this)
+                });
+                ReactDOM.render(elem, this);
             }
-        }, 50, { leading: false, trailing: true });
-
-        this._dispatchResponseChanged = debounce(() => {
-
-            var event = new CustomEvent('session-changed', {
-                bubbles: true,
-                composed: true,
-                detail: {
-                    complete: this.isComplete(),
-                    component: this.tagName.toLowerCase()
-                }
-            });
-
-            this.dispatchEvent(event);
-        });
-
-        this._dispatchModelSet = debounce(() => {
-            this.dispatchEvent(new CustomEvent('model-set', {
-                bubbles: true,
-                composed: true,
-                detail: {
-                    complete: this.isComplete(),
-                    component: this.tagName.toLowerCase(),
-                    hasModel: this._model !== undefined
-                }
-            }));
-        }, 50, { leading: false, trailing: true });
+        };
     }
 
-    set model(s) {
-        this._model = s;
+    set model(m) {
+        this._model = m;
+        this.dispatchEvent(
+            new ModelSetEvent(
+                this.tagName.toLowerCase(),
+                this.session && !!this.session.value,
+                !!this._model
+            )
+        );
+
         this._rerender();
-        this._dispatchModelSet();
+    }
+
+    set session(s) {
+        this._session = s;
+        this._rerender();
     }
 
     get session() {
         return this._session;
     }
 
-    set session(s) {
-        this._session = s;
+    _handleChoiceChange(selectedChoice) {
+        this.session.value = selectedChoice;
+        this.dispatchEvent(
+            new SessionChangedEvent(
+                this.tagName.toLowerCase(),
+                this.session && !!this.session.value
+            )
+        );
         this._rerender();
-        this._dispatchResponseChanged();
-    }
-
-    _onChange(data) {
-        this._session.value =  this._session.value || [];
-        if (data.selected) {
-            this._session.value = [data.value]
-        } else {
-            this._session.value = [];
-        }
-        this._dispatchResponseChanged();
-        this._rerender();
-    }
-
-    isComplete() {
-        const { complete } = this._model;
-        if (complete) {
-            const { min = -1, max = -1 } = complete;
-            const choiceCount = this._session && this._session.value ? this._session.value.length : 0;
-            const overMin = min === -1 || choiceCount >= min;
-            const underMax = max === -1 || choiceCount <= max;
-            return overMin && underMax;
-        } else {
-            return true;
-        }
     }
 
     connectedCallback() {
         this._rerender();
     }
-
-
 }
--- packages/likert-choice/src/main.jsx
+++ packages/likert-choice/src/main.jsx
@@ -1,9 +1,87 @@
 import React from 'react';
-import LikertChoice from './likert-choice';
+import Radio from 'material-ui/Radio';
+import Typography from 'material-ui/typography';
+import {withStyles} from 'material-ui/styles';
 
-const Main = (props) => {
-    const { model, onChoiceChanged, session, classes } = props;
-    return <LikertChoice {...model} session={session} onChoiceChanged={onChoiceChanged}/>
+const styles = {
+    radioButton: {
+        marginBottom: 16,
+    },
+    container: {
+        verticalAlign: 'middle'
+    },
+    row: {
+        display: 'inline-block',
+        textAlign: 'center',
+        paddingRight: '50px',
+        paddingLeft: '50px'
+    },
+    textRow: {
+        display: 'block'
+    },
+    identifier: {
+        display: 'inline-flex',
+        paddingRight: '1em',
+        fontSize: '1em',
+        fontWeight: 'bold'
+    },
+    label: {
+        display: 'inline-flex',
+    }
+};
+
+class Main extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state= {
+            radioValue: ''
+        }
+    }
+
+    label(nameKey, myArray){
+        for (var i=0; i < myArray.length; i++) {
+            if (myArray[i].lang === nameKey) {
+                return <div className={this.props.classes.label} dangerouslySetInnerHTML={{__html: myArray[i].label}}/>;
+            }
+        }
+    }
+
+    indexToSymbol(index) {
+        return (this.props.model.keyMode === 'numbers'
+                ? index + 1
+                : String.fromCharCode(97 + index).toUpperCase()
+        ).toString();
+    }
+
+    toggleRadio(e){
+        this.props.onChoiceChanged(e);
+        this.setState({radioValue: e.target.value});
+    }
+
+
+    render(){
+        const {classes, model} = this.props;
+        const {radioValue} = this.state;
+        return (
+            <div>
+                <div dangerouslySetInnerHTML={{__html: model.prompt}}/>
+                <div className={classes.container}>
+                    {model.choices.map((k,index) => {
+                        return(
+                            <div key={index} className={classes.row}>
+                                <Radio name='likert' onClick={(e) => this.toggleRadio(e)} checked={radioValue === k.value} value={k.value}/>
+                                <div className={classes.textRow}>
+                                    <Typography className={classes.identifier}>{this.indexToSymbol(index)}.</Typography>
+                                    {this.label(model.activeLang, k.label)}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Main;
+export default withStyles(styles)(Main);

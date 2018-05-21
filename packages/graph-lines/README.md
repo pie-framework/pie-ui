82b1376f28c6e5458b5efbe3f671034e771c2e51
Elod-Arpad Szopos <elodszopos@hotmail.com>
2018-05-21 14:56:18 +0300

    feature(graph-lines): initial structural commit, feature started

---
+++ packages/graph-lines/README.md
@@ -0,0 +1,4 @@
+# graph-lines
+
+This interaction asks a student to draw a line that meets specific criteria. The student will draw the line by clicking on two points on the graph.
+
---
+++ packages/graph-lines/src/controls.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Delete from '@material-ui/icons/Delete';
+
+export class Controls extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    onDeleteClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired
+  };
+
+  render() {
+    const { classes, disabled, onDeleteClick } = this.props;
+
+    return (
+      <Button
+        className={classes.button}
+        color="primary"
+        disabled={disabled}
+        onClick={onDeleteClick}
+      >
+        Delete
+        <Delete className={classes.rightIcon} />
+      </Button>
+    );
+  }
+}
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
+  }
+});
+
+export default withStyles(styles)(Controls);

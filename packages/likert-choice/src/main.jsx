574229062720ddf2e8c8ced347c819fe542c8420
Harry <hsharma@primoko.com>
2018-05-21 16:09:38 +0530

    material ui version updated to @material-ui/core

--- packages/likert-choice/src/main.jsx
+++ packages/likert-choice/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Radio from 'material-ui/Radio';
-import Typography from 'material-ui/typography';
-import {withStyles} from 'material-ui/styles';
+import Radio from '@material-ui/core/Radio';
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
 const styles = {

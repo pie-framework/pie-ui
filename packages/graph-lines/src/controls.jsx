99b17cb1b34aac2d04b88362c02ac20971930b08
Elod-Arpad Szopos <elodszopos@hotmail.com>
2018-06-06 14:36:17 +0300

    feature(graph-lines): finalized initial functionality of graph-lines with multiple lines included

--- packages/graph-lines/src/controls.jsx
+++ packages/graph-lines/src/controls.jsx
@@ -3,36 +3,44 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import Delete from '@material-ui/icons/Delete';
+import classnames from 'classnames';
 
 export class Controls extends React.Component {
   static propTypes = {
+    iconOnly: PropTypes.bool.isRequired,
     classes: PropTypes.object.isRequired,
     onDeleteClick: PropTypes.func.isRequired,
     disabled: PropTypes.bool.isRequired
   };
 
   render() {
-    const { classes, disabled, onDeleteClick } = this.props;
+    const { classes, disabled, onDeleteClick, iconOnly } = this.props;
 
     return (
       <Button
-        className={classes.button}
+        className={iconOnly ? classes.smallButton : classes.button }
         color="primary"
         disabled={disabled}
         onClick={onDeleteClick}
       >
-        Delete
-        <Delete className={classes.rightIcon} />
+        {!iconOnly && 'Delete'}
+        <Delete className={classnames(classes.icon, { [classes.smallIcon]: iconOnly })} />
       </Button>
     );
   }
 }
 
 const styles = theme => ({
+  smallButton: {
+    margin: 0,
+    padding: 0,
+    minWidth: 0,
+    width: '30px'
+  },
   button: {
     margin: theme.spacing.unit
   },
-  rightIcon: {
+  icon: {
     marginLeft: theme.spacing.unit
   }
 });

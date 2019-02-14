4062c9ec254a732e8ec9e3eeccb7970b357d9487
ed eustace <ed.eustace@gmail.com>
2019-02-14 22:48:38 +0000

    fix: test build

--- packages/calculator/src/index.js
+++ packages/calculator/src/index.js
@@ -5,7 +5,7 @@ import { CalculatorLayout } from './draggable-calculator';
 
 export { CalculatorLayout };
 
-// calculator
+// calculator?
 export default class Calculator extends HTMLElement {
   constructor() {
     super();

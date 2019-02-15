cd9d869f628305569c728ec539889863db724f8f
ed eustace <ed.eustace@gmail.com>
2019-02-15 10:54:12 +0000

    fix: minor clean up

--- packages/calculator/src/index.js
+++ packages/calculator/src/index.js
@@ -5,7 +5,6 @@ import { CalculatorLayout } from './draggable-calculator';
 
 export { CalculatorLayout };
 
-// calculator?
 export default class Calculator extends HTMLElement {
   constructor() {
     super();

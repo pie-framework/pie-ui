5e977a116f021a8027f1fa213027a4211ed2f8d2
ed eustace <ed.eustace@gmail.com>
2019-03-05 13:37:34 +0000

    tweak cl

--- scripts/cl.js
+++ scripts/cl.js
@@ -1,5 +1,11 @@
 var conventionalChangelog = require('conventional-changelog');
-
+const { resolve } = require('path');
 conventionalChangelog({
-  preset: 'angular'
+  preset: 'angular',
+  pkg: {
+    path: resolve(__dirname, '../packages/calculator')
+  },
+  outputUnreleased: true,
+  lernaPackage: '@pie-ui/calculator',
+  releaseCount: 1
 }).pipe(process.stdout); // or any writable stream

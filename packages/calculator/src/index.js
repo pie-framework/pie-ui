76e51bea929dcae9647b8fa5d7794e0cc8d35f66
ed eustace <ed.eustace@gmail.com>
2019-03-06 10:34:44 +0000

    fix: foo bar

--- packages/calculator/src/index.js
+++ packages/calculator/src/index.js
@@ -4,7 +4,6 @@ import Main from './main';
 import { CalculatorLayout } from './draggable-calculator';
 
 export { CalculatorLayout };
-// foo
 export default class Calculator extends HTMLElement {
   constructor() {
     super();

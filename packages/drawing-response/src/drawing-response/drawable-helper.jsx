f44703bda5829b783ccfefe4541b083e5238fcb3
Sergiu Lucaci <sergius.lucaci@gmail.com>
2019-05-31 19:51:32 +0300

    add a brief outline drawing-response related

--- packages/drawing-response/src/drawing-response/drawable-helper.jsx
+++ packages/drawing-response/src/drawing-response/drawable-helper.jsx
@@ -1,3 +1,8 @@
+/**
+ * Not using React in order to avoid performance issues.
+ * If you switch this class to a React one make sure that
+ * this interaction will not have performance leaks in pie-website nor in pits.
+ */
 export default class DrawableHelper {
   constructor(props) {
     const { startx, starty, fillColor, outlineColor } = props;

3f3a9fce5f6c4545cea1ac6875a2e44d4bc216a2
andreeapescar <andreeaflaviapescar@gmail.com>
2019-06-19 23:09:22 -0400

    feat: save response on session, added graph representation

--- packages/graphing/src/index.js
+++ packages/graphing/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main';
 import debug from 'debug';
+import { SessionChangedEvent } from '@pie-framework/pie-player-events';
 
 const log = debug('pie-ui:graphing');
 
@@ -31,6 +32,26 @@ export default class GraphLines extends HTMLElement {
     this._render();
   }
 
+  isComplete() {
+    if (!this._session) {
+      return false;
+    }
+
+    return (
+      Array.isArray(this._session.answers) && this._session.answers.length > 0
+    );
+  }
+
+  changeAnswers(answers) {
+    this._session.answers = answers;
+
+    this.dispatchEvent(
+      new SessionChangedEvent(this.tagName.toLowerCase(), this.isComplete())
+    );
+
+    this._render();
+  }
+
   _render() {
     if (!this._model || !this._session) {
       return;
@@ -39,7 +60,8 @@ export default class GraphLines extends HTMLElement {
     const el = React.createElement(Main, {
       model: this._model,
       session: this._session,
-      onSessionChange: this.sessionChanged.bind(this)
+      onSessionChange: this.sessionChanged.bind(this),
+      onAnswersChange: this.changeAnswers.bind(this),
     });
 
     ReactDOM.render(el, this);
--- packages/graphing/src/main.jsx
+++ packages/graphing/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { GraphContainer as Graph, tools } from '@pie-lib/graphing';
+import { marks } from './demo-data';
 
 export class Main extends React.Component {
   static propTypes = {
@@ -10,9 +12,92 @@ export class Main extends React.Component {
     model: PropTypes.object.isRequired
   };
 
+  constructor(props) {
+    super(props);
+
+    // console.log('tools:', tools.lineTwo);
+
+    const toolsArr = [
+      tools.point(),
+      tools.circle(),
+      tools.polygon(),
+      tools.segment(),
+      tools.vector(),
+      tools.ray(),
+      tools.line(),
+      tools.sine(),
+      tools.parabola()
+    ];
+    this.state = {
+      currentTool: toolsArr[2],
+      tools: toolsArr,
+      displayedTools: toolsArr,
+      settings: {
+        includeArrows: true,
+        labels: true,
+        graphTitle: false,
+        coordinatesOnHover: false,
+        size: {
+          width: 400,
+          height: 400
+        }
+      },
+      model: {
+        title: undefined,
+        domain: {
+          min: -10,
+          max: 10,
+          padding: 0,
+          step: 1,
+          labelStep: 1
+        },
+        range: {
+          min: -5,
+          max: 5,
+          padding: 0,
+          step: 1,
+          labelStep: 1
+        },
+        backgroundMarks: (props.model && props.model.backgroundMarks) || [],
+        marks
+      }
+    };
+  }
+
+  changeMarks = marks => {
+    const model = { ...this.state.model, marks };
+
+    this.setState({ model });
+    this.props.onAnswersChange(marks);
+  };
+
   render() {
+    const { settings, model, tools } = this.state;
+    const { model: { backgroundMarks, displayedTools = tools, correctMarks } } = this.props;
+
+    // TODO check which one should be used (with alternates)
+    const correctAnswerMarks = (correctMarks && correctMarks.correctAnswer) || null;
+
     return (
-      <div />
+      <div>
+        <Graph
+          size={settings.size}
+          domain={model.domain}
+          range={model.range}
+          title={model.title}
+          axesSettings={{ includeArrows: settings.includeArrows }}
+          labels={settings.labels && model.labels}
+          marks={model.marks}
+          correctnessMarks={correctAnswerMarks}
+          backgroundMarks={backgroundMarks}
+          onChangeMarks={this.changeMarks}
+          displayedTools={displayedTools}
+          tools={tools}
+          currentTool={displayedTools[0]}
+          defaultTool={displayedTools[0].type}
+        />
+
+      </div>
     );
   }
 }

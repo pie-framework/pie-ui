5bbd4eec944f1d2d1f1a845ec94f99b7a481956b
lakatosandrei <lakatosandrei@gmail.com>
2019-07-25 17:26:58 +0300

    chore(jest): updated jest config in order to be able to test with react-dnd

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testRegex: 'src/.*/?__tests__/.*.test\\.jsx?$',
-  testURL: 'http://localhost/'
+  testURL: 'http://localhost/',
+  moduleNameMapper: {
+    '^dnd-core$': 'dnd-core/dist/cjs',
+    '^react-dnd$': 'react-dnd/dist/cjs',
+    '^react-dnd-html5-backend$': 'react-dnd-html5-backend/dist/cjs',
+    '^react-dnd-touch-backend$': 'react-dnd-touch-backend/dist/cjs',
+    '^react-dnd-test-backend$': 'react-dnd-test-backend/dist/cjs',
+    '^react-dnd-test-utils$': 'react-dnd-test-utils/dist/cjs'
+  }
 };

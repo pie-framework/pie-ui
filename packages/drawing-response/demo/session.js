4af2d2d902dc9e7cb203b095a14f32ca6f376c35
ed eustace <ed.eustace@gmail.com>
2020-01-21 11:55:00 +0000

    fix: render value first, fallback to label [ch6084]

--- packages/drawing-response/demo/session.js
+++ packages/drawing-response/demo/session.js
@@ -773,6 +773,36 @@ const threeLinesRect = [
 const sample = {
   id: '1',
   element: 'drawing-response',
+  texts: [
+    {
+      width: 200,
+      textVisible: true,
+      type: 'text-entry',
+      label: 'Double click to edit this text. Press Enter to submit.',
+      scaleX: 1,
+      y: 0,
+      skewX: 0,
+      name: 'text_wyut0wszhvok5nrt9zt',
+      scaleY: 1,
+      x: 0,
+      fill: 'black',
+      offsetY: 0,
+      bubbles: true,
+      draggable: true,
+      offsetX: 0,
+      isDefault: false,
+      skewY: 0,
+      visible: true,
+      text: 'ed test 1',
+      rotation: 0,
+      id: 'wyut0wszhvok5nrt9zt',
+      value: 'ed test 1',
+      createdAt: '2020-01-21T11:03:51.257Z',
+      fontSize: 16,
+      textareaVisible: false,
+      transformerVisible: false
+    }
+  ],
   drawables: [
     {
       startx: 134,

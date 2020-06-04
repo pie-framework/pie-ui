6f687e048a12428f2bb0e86d23aeba7645d57ff9
ed eustace <ed.eustace@gmail.com>
2020-06-04 16:58:30 +0100

    get things set up

--- packages/multiple-choice/demo/config.js
+++ packages/multiple-choice/demo/config.js
@@ -6,7 +6,7 @@ module.exports = {
     {
       element: 'multiple-choice',
       id: '1',
-      mode: 'evaluate',
+      mode: 'gather',
       prompt: 'I have $4 and you have $5 - who has more?',
       disabled: false,
       choiceMode: 'radio',
@@ -24,6 +24,32 @@ module.exports = {
         {
           label: 'banana <span data-latex="">\\(\\frac{1}{2}\\)</span>',
           value: 'b',
+        },
+      ],
+    },
+    {
+      element: 'multiple-choice',
+      id: '1',
+      mode: 'evaluate',
+      prompt: 'I have $4 and you have $5 - who has more?',
+      disabled: true,
+      choiceMode: 'radio',
+      keyMode: 'letters',
+      responseCorrect: false,
+      // correctResponse: [{ value: 'b' }],
+
+      choices: [
+        {
+          label: 'apple <span data-latex="">$\\frac{2}{2}$</span>',
+          value: 'a',
+          correct: false,
+          feedback: 'feedback',
+        },
+        {
+          label: 'banana <span data-latex="">\\(\\frac{1}{2}\\)</span>',
+          value: 'b',
+          correct: true,
+          feedback: 'feedback',
         },
       ],
     },
--- packages/multiple-choice/src/main.jsx
+++ packages/multiple-choice/src/main.jsx
@@ -9,47 +9,47 @@ import classNames from 'classnames';
 import { PreviewLayout } from '@pie-lib/render-ui';
 import MultipleChoice from './multiple-choice';
 
-const styleSheet = theme => {
-  const root = {
-    '--feedback-correct-bg-color': green[500],
-    '--feedback-incorrect-bg-color': orange[500],
-    '--feedback-color': 'black',
-    '--choice-input-color': 'black',
-    '--choice-input-selected-color': theme.palette.primary[500],
-    '--choice-input-disabled-color': theme.palette.grey[500],
-    '--choice-input-correct-disabled-color': green[500],
-    '--choice-input-incorrect-disabled-color': orange[500],
-    backgroundColor: 'var(--mc-bg-color, rgba(0,0,0,0))'
-  };
+// const styleSheet = theme => {
+//   const root = {
+//     '--feedback-correct-bg-color': green[500],
+//     '--feedback-incorrect-bg-color': orange[500],
+//     '--feedback-color': 'black',
+//     '--choice-input-color': 'black',
+//     '--choice-input-selected-color': theme.palette.primary[500],
+//     '--choice-input-disabled-color': theme.palette.grey[500],
+//     '--choice-input-correct-disabled-color': green[500],
+//     '--choice-input-incorrect-disabled-color': orange[500],
+//     backgroundColor: 'var(--mc-bg-color, rgba(0,0,0,0))'
+//   };
 
-  return {
-    root,
-    'white-on-black': {
-      '--correct-answer-toggle-label-color': 'white',
-      '--feedback-correct-bg-color': green[800],
-      '--feedback-incorrect-bg-color': orange[800],
-      '--feedback-color': 'white',
-      '--mc-bg-color': 'black',
-      '--choice-input-color': 'white',
-      '--choice-input-selected-color': theme.palette.primary[200]
-    },
-    'black-on-rose': {
-      '--mc-bg-color': pink[50]
-    }
-  };
-};
+//   return {
+//     root,
+//     'white-on-black': {
+//       '--correct-answer-toggle-label-color': 'white',
+//       '--feedback-correct-bg-color': green[800],
+//       '--feedback-incorrect-bg-color': orange[800],
+//       '--feedback-color': 'white',
+//       '--mc-bg-color': 'black',
+//       '--choice-input-color': 'white',
+//       '--choice-input-selected-color': theme.palette.primary[200]
+//     },
+//     'black-on-rose': {
+//       '--mc-bg-color': pink[50]
+//     }
+//   };
+// };
 
 class Main extends React.Component {
   static propTypes = {
     model: PropTypes.object,
     session: PropTypes.object,
     onChoiceChanged: PropTypes.func,
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
   };
 
   static defaultProps = {
     model: {},
-    session: {}
+    session: {},
   };
   constructor(props) {
     super(props);
@@ -74,15 +74,15 @@ class Main extends React.Component {
   }
 }
 
-const Styled = withStyles(styleSheet, { name: 'Main' })(Main);
+const Styled = withStyles({}, { name: 'Main' })(Main);
 
 const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
-  }
+  },
 });
 
-const Root = props => (
+const Root = (props) => (
   <MuiThemeProvider theme={theme}>
     <Styled {...props} />
   </MuiThemeProvider>

68a1d51fbbf8abe1a902e6144291df767f149b71
Elod Szopos <elodszopos@gmail.com>
2020-06-17 14:04:56 +0300

    feat(likert): initial commit

---
+++ packages/likert/demo/config.js
@@ -0,0 +1,24 @@
+const base = {};
+
+const model = (id, extras) =>
+  Object.assign(
+    {},
+    base,
+    {
+      id,
+      element: 'likert-el',
+      backgroundMarks: [],
+      size: { width: 600, height: 600 },
+      domain: { min: -5, max: 5, padding: 0, step: 1, labelStep: 1 },
+      range: { min: -5, max: 5, padding: 0, step: 1, labelStep: 1 },
+      toolbarTools: ['point']
+    },
+    extras
+  );
+
+module.exports = {
+  elements: {
+    'likert-el': '..'
+  },
+  models: [model('1', {})]
+};
---
+++ packages/likert/src/main.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { GraphContainer } from '@pie-lib/graphing';
+import { Collapsible } from '@pie-lib/render-ui';
+import CorrectAnswerToggle from '@pie-lib/correct-answer-toggle';
+
+export class Main extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object,
+    model: PropTypes.object.isRequired,
+    session: PropTypes.object.isRequired,
+    onAnswersChange: PropTypes.func
+  };
+
+  static defaultProps = {
+    classes: {}
+  };
+
+  state = { showingCorrect: false };
+
+  toggleCorrect = showingCorrect => this.setState({ showingCorrect });
+
+  render() {
+    const { model, classes, onAnswersChange, session } = this.props;
+    const { showingCorrect } = this.state;
+    const { answer } = session || {};
+    const {
+      answersCorrected,
+      arrows,
+      backgroundMarks,
+      correctResponse,
+      disabled,
+      domain,
+      labels,
+      prompt,
+      range,
+      rationale,
+      size,
+      showToggle,
+      title,
+      teacherInstructions,
+      toolbarTools
+    } = model || {};
+
+    const marks = answersCorrected || answer || [];
+
+
+    return (
+      <div>
+        <CorrectAnswerToggle
+          show={showToggle}
+          toggled={showingCorrect}
+          onToggle={this.toggleCorrect}
+        />
+
+        {(showingCorrect && showToggle) && (
+          <GraphContainer
+            axesSettings={{ includeArrows: arrows }}
+            backgroundMarks={backgroundMarks}
+            disabled={true}
+            domain={domain}
+            labels={labels}
+            marks={correctResponse}
+            onChangeMarks={onAnswersChange}
+            range={range}
+            size={size}
+            title={title}
+            toolbarTools={toolbarTools}
+          />
+        )}
+
+        {
+          teacherInstructions && (
+            <Collapsible labels={{ hidden: 'Show Teacher Instructions', visible: 'Hide Teacher Instructions' }}>
+              <div dangerouslySetInnerHTML={{ __html: teacherInstructions }}/>
+            </Collapsible>
+          )
+        }
+
+        <br/>
+
+        <div className={classes.prompt} dangerouslySetInnerHTML={{ __html: prompt }}/>
+
+        <br/>
+
+        <GraphContainer
+          axesSettings={{ includeArrows: arrows }}
+          backgroundMarks={backgroundMarks}
+          disabled={disabled}
+          domain={domain}
+          labels={labels}
+          marks={marks}
+          onChangeMarks={onAnswersChange}
+          range={range}
+          size={size}
+          title={title}
+          toolbarTools={toolbarTools}
+        />
+
+        <br/>
+
+        {
+          rationale && (
+            <Collapsible labels={{ hidden: 'Show Rationale', visible: 'Hide Rationale' }}>
+              <div dangerouslySetInnerHTML={{ __html: rationale }}/>
+            </Collapsible>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+const styles = () => ({
+  prompt: { verticalAlign: 'middle' }
+});
+
+export default withStyles(styles)(Main);

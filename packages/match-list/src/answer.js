267b547925fcee9afe915c68fe06ce9c9d83d98d
Elod Szopos <elodszopos@gmail.com>
2020-09-10 14:21:46 +0300

    feat(match-list): use render ui colors

--- packages/match-list/src/answer.js
+++ packages/match-list/src/answer.js
@@ -7,6 +7,7 @@ import debug from 'debug';
 import { withStyles } from '@material-ui/core/styles';
 import { PlaceHolder } from '@pie-lib/drag';
 import isEmpty from 'lodash/isEmpty';
+import { color } from '@pie-lib/render-ui';
 
 const log = debug('pie-elements:match-title:answer');
 
@@ -59,14 +60,14 @@ const AnswerContent = withStyles({
     opacity: 0.5
   },
   disabled: {
-    backgroundColor: '#ececec',
+    backgroundColor: color.background(),
     cursor: 'not-allowed'
   },
   incorrect: {
-    border: 'solid 1px orange'
+    border: `1px solid ${color.incorrect()}`
   },
   correct: {
-    border: 'solid 1px green'
+    border: `1px solid ${color.correct()}`
   }
 })(props => {
   const {
@@ -173,10 +174,10 @@ const StyledAnswer = withStyles({
     textAlign: 'center'
   },
   incorrect: {
-    border: '1px solid var(--feedback-incorrect-bg-color, orange)'
+    border: `1px solid var(--feedback-incorrect-bg-color, ${color.incorrect()})`
   },
   correct: {
-    border: '1px solid var(--feedback-correct-bg-color, green)'
+    border: `1px solid var(--feedback-correct-bg-color, ${color.correct()})`
   }
 })(Answer);
 

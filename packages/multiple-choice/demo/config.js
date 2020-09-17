a0d641a5999a4bc77cc08476c36e42eca9056901
Elod Szopos <elodszopos@gmail.com>
2020-09-17 21:46:20 +0300

    chore: update multiple choice demo and finalize changes

--- packages/multiple-choice/demo/config.js
+++ packages/multiple-choice/demo/config.js
@@ -53,5 +53,31 @@ module.exports = {
         },
       ],
     },
+    {
+      element: 'multiple-choice',
+      id: '3',
+      mode: 'gather',
+      prompt: 'I have $4 and you have $5 - who has more?',
+      disabled: true,
+      choiceMode: 'checkbox',
+      keyMode: 'letters',
+      responseCorrect: false,
+      // correctResponse: [{ value: 'b' }],
+
+      choices: [
+        {
+          label: 'apple <span data-latex="">$\\frac{2}{2}$</span>',
+          value: 'a',
+          correct: false,
+          feedback: 'feedback',
+        },
+        {
+          label: 'banana <span data-latex="">\\(\\frac{1}{2}\\)</span>',
+          value: 'b',
+          correct: true,
+          feedback: 'feedback',
+        },
+      ],
+    },
   ],
 };

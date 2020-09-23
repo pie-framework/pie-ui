5f1d8746c41509eaa2908147a473546fe771aaba
Elod Szopos <elodszopos@gmail.com>
2020-09-23 08:21:41 +0300

    feat(match): color theme support added

--- packages/match/demo/config.js
+++ packages/match/demo/config.js
@@ -1,42 +1,52 @@
 const base = {
   id: '1',
   element: 'match-element',
-  rows: [{
-    id: 1,
-    title: 'Question Text 1',
-    values: [false, false]
-  }, {
-    id: 2,
-    title: 'Question Text 2',
-    values: [false, false]
-  }, {
-    id: 3,
-    title: 'Question Text 3',
-    values: [false, false]
-  }, {
-    id: 4,
-    title: 'Question Text 4',
-    values: [false, false]
-  }],
-  shuffled: false,
-  partialScoring: [],
-  layout: 3,
-  headers: ['Column 1', 'Column 2', 'Column 3'],
-  responseType: 'radio',
-  feedback: {
-    correct: {
-      type: 'none',
-      default: 'Correct'
+  config: {
+    enableImages: true,
+    feedback: {
+      correct: {
+        type: 'none',
+        default: 'Correct'
+      },
+      partial: {
+        type: 'none',
+        default: 'Nearly'
+      },
+      incorrect: {
+        type: 'none',
+        default: 'Incorrect'
+      }
     },
-    partial: {
-      type: 'none',
-      default: 'Nearly'
-    },
-    incorrect: {
-      type: 'none',
-      default: 'Incorrect'
-    }
-  },
+    headers: ['Column 1', 'Column 2', 'Column 3'],
+    layout: 3,
+    lockChoiceOrder: true,
+    partialScoring: false,
+    choiceMode: 'radio',
+    rows: [
+      {
+        id: 1,
+        title: 'Question Text 1',
+        values: [false, false]
+      },
+      {
+        id: 2,
+        title: 'Question Text 2',
+        values: [false, false]
+      },
+      {
+        id: 3,
+        title: 'Question Text 3',
+        values: [false, false]
+      },
+      {
+        id: 4,
+        title: 'Question Text 4',
+        values: [false, false]
+      }
+    ],
+    promptEnabled: true,
+    prompt: 'Select correct answers.',
+  }
 };
 
 const model = (id, extras) =>

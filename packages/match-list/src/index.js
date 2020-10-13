8ba2d52a053c821456ba9f000f3b087bca3af660
DreamerIn <chetubathi07@gmail.com>
2020-10-13 16:07:11 +0530

    fix: complete is false if 0 is part of session value PD-541

--- packages/match-list/src/index.js
+++ packages/match-list/src/index.js
@@ -20,7 +20,7 @@ export const isComplete = (session, model) => {
 
   if (value) {
     (model.config.prompts || []).forEach(prompt => {
-      if (!value[prompt.id] && value[prompt.id] != 0) {
+      if (!Number.isFinite(value[prompt.id])) {
         complete = false;
       }
     })
